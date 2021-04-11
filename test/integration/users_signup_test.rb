d49dcdac85613259c34b2ce408c27698808e601d
Byron Pearce <byront-800@protonmail.com>
2021-04-11 11:53:54 -0300

    Add user signup form

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup info should be invalid" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '', email: 'foo@bar', password: 'foo', password_confirmation: 'bar'} }
+    end
+    assert_template 'users/new'
+  end
+end
